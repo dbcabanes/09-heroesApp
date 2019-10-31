1075e54d6b1d2c6d6795e5ec5c578e28625c83e1
dbcabanes <dborrascabanes@hotmail.com>
2019-10-31 13:54:42 +0100

    Ya agregamos campos a firebase

---
+++ src/app/services/heroes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HeroeModel } from "../models/heroe.model";
+import { map } from "rxjs/operators";
+import { EROFS } from "constants";
+
+@Injectable({
+  providedIn: "root"
+})
+export class HeroesService {
+  private url = "https://login-app-4d9d1.firebaseio.com";
+
+  constructor(private http: HttpClient) {}
+
+  crearHeroe(heroe: HeroeModel) {
+    return this.http.post(`${this.url}/heroes.json`, heroe).pipe(
+      map((resp: any) => {
+        heroe.id = resp.name;
+        return heroe;
+      })
+    );
+  }
+}
